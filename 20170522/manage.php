a17df182b8336009f7b5dd6b326ad0d4d39837d2
t-nozomu <dressbreak@gmail.com>
2017-05-25 18:03:05 +0900

    20170525

--- 20170522/manage.php
+++ 20170522/manage.php
@@ -7,28 +7,72 @@
     <body>
         <h1>管理表</h1>
         <div class = "g">
-            <div class = "r">お問い合わせ番号</div>
-            <div class = "r">氏名</div>
-            <div class = "r">性別</div>
-            <div class = "r">住所</div>
-            <div class = "r">電話番号</div>
-            <div class = "r">メールアドレス</div>
-            <div class = "r">どこで知ったか</div>
-            <div class = "r">質問カテゴリ</div>
-            <div class = "r">質問内容</div>
+            <div class = "num">お問い合わせ番号</div>
+            <div class = "name">氏名</div>
+            <div class = "s">性別</div>
+            <div class = "add">住所</div>
+            <div class = "phone">電話番号</div>
+            <div class = "acount">メールアドレス</div>
+            <div class = "info">どこで知ったか</div>
+            <div class = "cate">質問カテゴリ</div>
+            <div class = "msg">質問内容</div>
         </div>
 
         <?php
+
             $fp = fopen("manage_list.csv","r");
         while (($line = fgetcsv($fp)) !== false){
             echo '<div class = "g" >';
-            foreach($line as $line){
-                echo '<div class = "r">';
-                echo $line;
+            for($i = 0; $i < count($line); $i++ ){
+                switch($i){
+                   case 0:
+                        echo '<div class = "num">';
+                        break;
+
+                    case 1:
+                        echo '<div class = "name">';
+                        break;
+
+                    case 2:
+                        echo '<div class = "s">';
+                        break;
+
+                    case 3:
+                        echo '<div class = "add">';
+                        break;
+
+                    case 4:
+                        echo '<div class = "phone">';
+                        break;
+
+                    case 5:
+                        echo '<div class = "acount">';
+                        break;
+
+                    case 6:
+                        echo '<div class = "info">';
+                        break;
+
+                    case 7:
+                        echo '<div class = "cate">';
+                        break;
+
+                    case 8:
+                        echo '<div class = "msg">';
+                        break;
+
+                    default:
+                        echo '<div class = "r">';
+                        break;
+
+                }
+                echo $line[$i];
                 echo "</div>";
             }
             echo "</div>";
+
         }
+
         ?>
 
     </body>
--- 20170522/web.php
+++ 20170522/web.php
@@ -6,41 +6,42 @@
     <head>
         <meta charset = "utf-8">
         <title>お問い合わせフォーム</title>
+        <link rel = "stylesheet" type = "text/css" href = "style.css">
     </head>
         <body>
-            <h1><div Align = "center" > お問い合わせ内容確認　</div></h1>
+            <h1><div Align = "center" > お問い合わせ内容確認</div></h1>
                 <form action = "comp.php" method = "post">
-                    <table border = "0" align = "center" >　
-                    <tr>
-                        <td>姓</td>
-                        <td>
+                <div class = "dd">
+                    <div class = "d">
+                        <div class = "d1">姓</div>
+                        <div class = "d2">
                             <?php
                                 $lname = $_POST['Lastname'];
                                 echo $lname;
                              ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>名</td>
-                        <td>
+                        </div>
+                    </div>
+                    <div class = "d">
+                        <div class = "d1">名</div>
+                        <div class = "d2">
                             <?php
                                 $fname = $_POST['Firstname'];
                                 echo $fname;
                              ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>性別</td>
-                        <td>
+                        </div>
+                    </div>
+                    <div class  = "d">
+                        <div class = "d1">性別</div>
+                        <div class = "d2">
                             <?php
                                     $s = $_POST['s'];
                                     echo $s;
                              ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>住所</td>
-                        <td>
+                        </div>
+                    </div>
+                    <div class  = "d">
+                        <div class = "d1">住所</div>
+                        <div class = "d2">
                             <?php
                                 if(isset($_POST['address'])){
                                     $add = $_POST['address'];
@@ -48,33 +49,33 @@
 
                                 }
                              ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>電話番号</td>
-                        <td>
+                        </div>
+                    </div>
+                    <div class  = "d">
+                        <div class = "d1">電話番号</div>
+                        <div class = "d2">
                             <?php
 
                                 $phone = $_POST['phone'];
                                 echo $phone[0]."-".$phone[1]."-".$phone[2] ;
 
                              ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>メールアドレス</td>
-                        <td>
+                        </div>
+                    </div>
+                    <div class = "d">
+                        <div class = "d1">メールアドレス</div>
+                        <div class = "d2">
                             <?php
 
                                 $acount = $_POST['acount'];
                                     echo $acount[0]."@".$acount[1];
 
                              ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>どこで知ったか</td>
-                        <td>
+                        </div>
+                    </div>
+                    <div class = "d">
+                        <div class = "d1">どこで知ったか</div>
+                        <div class = "d2">
                             <?php
                                 if(isset($_POST['info'])){
                                     $info = $_POST['info'];
@@ -83,11 +84,11 @@
                                     }
                                 }
                              ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>質問カテゴリ</td>
-                        <td>
+                        </div>
+                    </div>
+                    <div class = "d">
+                        <div class = "d1">質問カテゴリ</div>
+                        <div class = "d2">
                             <?php
 
                                 if(isset($_POST['category'])){
@@ -95,31 +96,30 @@
                                     echo $_POST['category'];
                                 }
                             ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>質問内容</td>
-                        <td >
+                        </div>
+                    </div>
+                    <div class = "d">
+                        <div class = "d1">質問内容</div>
+                        <div class = "d2">
                             <?php
                                 if(isset($_POST['msg'])){
                                     $msg = $_POST['msg'];
-                                    echo $_POST['msg'];
+                                    $msg = str_replace("\r\n","<br>", $msg);
+                                    echo $msg;
                                 }
                              ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan = "2">
+                        </div>
+                    </div>
+                    <div class = "d">
+                        <div>
                             <input type ="submit" value = "送信" >
                             <input type ="button" value = "戻る" onclick = "history.back()">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td = colspan = "2">
+                        </div>
+                    </div>
+                    <div class = "d">
                             <a href = "index.html">未入力状態でお書き直しします。</a>
-                        </td>
-                    </tr>
-                    </table>
+                    </div>
+                </div>
 
 
                     <input type = "hidden" name = "num" value = "<?php echo $number ;?>">
@@ -129,7 +129,7 @@
                     <input type = "hidden" name= "address" value = "<?php  echo $add;?>">
                     <input type = "hidden" name= "phone" value = "<?php echo $phone[0]."-".$phone[1]."-".$phone[2]; ?>">
                     <input type = "hidden" name= "acount" value = "<?php  echo $acount[0]."@".$acount[1];?>">
-                    <input type = "hidden" name= "info" value = "<?php  foreach($info as $value) echo "$value ";?>">
+                    <input type = "hidden" name= "info" value = "<?php  foreach($info as $value) echo "$value";?>">
                     <input type = "hidden" name= "category" value = "<?php  echo $cate;?>">
                     <input type = "hidden" name= "msg" value = "<?php  echo $msg;?>">
 
